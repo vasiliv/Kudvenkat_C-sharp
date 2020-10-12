f3acd4e0766b7dd208410f2c1342cbe1cdbcac6e
grandiosa iva <ggo24@yahoo.com>
2020-10-12 14:32:34 +0400

    Add project files.

---
+++ 17_methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// დავწეროთ ისეთი მეთოდი რომელიც იღებს მთელ არგუმენტს და იმ არგუმენტის ჩათვლით
+// ამობეჭდავს ლუწ ციფრებს
+
+
+namespace _17_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EvenNumbers(30);
+            Console.ReadLine();
+        }
+        static void EvenNumbers(int j)
+        {
+            for (int i = 0; i <=j; i+=2)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+    }
+}
