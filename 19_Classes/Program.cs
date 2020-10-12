41f6c8cc0363fdf47229f2f07d33ac9690f9ce21
grandiosa iva <ggo24@yahoo.com>
2020-10-12 15:45:38 +0400

    19

---
+++ 19_Classes/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Class customer. firstName, lastName. გადავცეთ default კონსტრუქტორიდან user defined
+// კონსტრუქტორს this keyword-ის გამოყენებით და ამოვბეჭდოთ
+
+namespace _19_Classes
+{
+    class Customer
+    {
+        private string _firstName, _lastName;
+        public Customer() : this ("Vasil", "Ivanishvili")
+        {
+
+        }
+        public Customer(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+        public void PrintCustomer()
+        {
+            Console.WriteLine("Customer name is: {0}, {1}", _firstName, _lastName);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer();
+            customer.PrintCustomer();
+            Console.ReadLine();
+        }
+    }
+}
