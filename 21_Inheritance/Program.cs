86b5e209df51f445e2a7b2800632448919f4f5b2
grandiosa iva <ggo24@yahoo.com>
2020-11-05 16:24:06 +0400

    21

---
+++ 21_Inheritance/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_Inheritance
+{
+    class Parent
+    {
+        public Parent()
+        {
+            Console.WriteLine("default parent constructor");
+        }
+        public Parent(string parent)
+        {
+            Console.WriteLine(parent);
+        }
+    }
+    class Child : Parent
+    {
+        public Child():base("Vasil")
+        {
+            Console.WriteLine("default child constructor");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Child petre = new Child();
+            Console.ReadLine();
+        }
+    }
+}
