a2612760beb015bcd6caa0f3fd1d90de22067ead
grandiosa iva <ggo24@yahoo.com>
2020-11-08 11:48:52 -0800

    27

---
+++ 27_Properties/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_Properties
+{
+    class Student
+    {
+        private int _id;
+        private string _name;
+
+        public int Id
+        {
+            set { 
+                if (value < 0)
+                {
+                    throw new Exception("less than zero");
+                }
+                else
+                {
+                    this._id = value;
+                }
+            }
+            get
+            {
+                return this._id;
+            }
+        }
+        public string Name
+        {
+            set { 
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("null");
+                }
+                else
+                {
+                    this._name = value;
+                }
+            }
+            get
+            {
+                return this._name;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            //student.Id = -10;
+            student.Id = 10;
+            Console.WriteLine("Student id = {0}", student.Id);
+            //student.Name = null;
+            student.Name = "Vaso";
+            Console.WriteLine("Student name = {0}", student.Name);
+            Console.ReadLine();
+        }
+    }
+}
