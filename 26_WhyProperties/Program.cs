94d1170e2f44d3633eb353bb0864ebf80bf72f8f
grandiosa iva <ggo24@yahoo.com>
2020-11-07 22:45:01 -0800

    26

---
+++ 26_WhyProperties/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// class student, id ar unda ikos 0-ze naklebi, name ar unda ikos nullis toli
+
+namespace _26_WhyProperties
+{
+    class Student
+    {
+        private int _id;
+        private string _name;
+
+        public void SetId(int id)
+        {
+            if (id < 0)
+            {
+                throw new Exception("less than zero");
+            }
+            else
+            {
+                this._id = id;
+            }
+        }
+        public int GetId()
+        {
+            return _id;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
