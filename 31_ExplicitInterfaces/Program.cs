20fb8a48cc0f89550089ad7471d80e0f3bb80cfa
grandiosa iva <ggo24@yahoo.com>
2020-11-08 04:17:54 -0800

    31

---
+++ 31_ExplicitInterfaces/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _31_ExplicitInterfaces
+{
+    interface I1
+    {
+        void InterfaceMethod();
+    }
+    interface I2
+    {
+        void InterfaceMethod();
+    }
+    class Program : I1, I2
+    {
+        void I1.InterfaceMethod()
+        {
+            Console.WriteLine("Interface1 method");
+        }
+        void I2.InterfaceMethod()
+        {
+            Console.WriteLine("Interface2 method");
+        }
+        static void Main(string[] args)
+        {
+            I1 program1 = new Program();
+            program1.InterfaceMethod();
+
+            in
+
+            Program program = new Program();
+            ((I2)program).InterfaceMethod();
+
+            Console.ReadKey();
+        }
+    }
+}
