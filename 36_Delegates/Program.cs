0ac9c426b3cb7baaff60c2d104e62fbedcdec473
grandiosa iva <ggo24@yahoo.com>
2020-11-09 11:00:18 +0400

    36

---
+++ 36_Delegates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// 
+
+namespace _36_Delegates
+{
+    public delegate void MyDelegate(string message);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDelegate myDelegate = new MyDelegate(Hello);
+            //Hello("Hello Vasil");
+            myDelegate("Hello Vasil");
+            Console.ReadLine();
+        }
+        public static void Hello(string strMessge)
+        {
+            Console.WriteLine(strMessge);
+        }
+    }
+}
