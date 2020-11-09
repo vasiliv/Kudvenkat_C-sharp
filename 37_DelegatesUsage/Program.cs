c26e0903be7a1833c4e7573c08cd3fa00a305bab
grandiosa iva <ggo24@yahoo.com>
2020-11-09 15:17:19 +0400

    38

--- 37_DelegatesUsage/Program.cs
+++ 37_DelegatesUsage/Program.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// 37 and 38
+
 // class Employee, autoimplemented properties Id, Name, Salary, Experience,
 // method PromoteEmployee which take list of employees as an argument, and returns nothing
 // foreach - if - experience > 5 - promote
 // main - create new list of employees and add some employees, call PromoteEmployee
 namespace _37_DelegatesUsage
 {
+    delegate bool IsPromotable(Employee employee);
     class Program
     {
         static void Main(string[] args)
@@ -18,9 +21,24 @@ namespace _37_DelegatesUsage
             employee.Add(new Employee { Id = 2, Name = "Lile", Salary = 5000, Experience = 2 });
             employee.Add(new Employee { Id = 3, Name = "Petre", Salary = 500, Experience = 1 });
 
-            Employee.PromoteEmployee(employee);
+            //Employee.PromoteEmployee(employee);
+            IsPromotable isPromotable = new IsPromotable(Promote);
+            Employee.PromoteEmployee(employee, isPromotable);
+
             Console.ReadLine();
         }
+        public static bool Promote(Employee emp)
+        {
+            if (emp.Experience > 5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
     }
     class Employee
     {
@@ -29,14 +47,24 @@ namespace _37_DelegatesUsage
         public int Salary { get; set; }
         public int Experience { get; set; }
 
-        public static void PromoteEmployee(List <Employee> emp)
+        //public static void PromoteEmployee(List <Employee> emp)
+        //{
+        //    foreach (Employee employee in emp)
+        //    {
+        //        if (employee.Experience > 5)
+        //        {
+        //            Console.WriteLine(employee.Name + " Promoted");
+        //        } 
+        //    }
+        //}
+        public static void PromoteEmployee(List<Employee> emp, IsPromotable isPromotable)
         {
             foreach (Employee employee in emp)
             {
-                if (employee.Experience > 5)
+                if (isPromotable(employee)) // delegate
                 {
                     Console.WriteLine(employee.Name + " Promoted");
-                } 
+                }
             }
         }
     }
